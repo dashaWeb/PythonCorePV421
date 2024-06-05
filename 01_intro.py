f450475ae5a4c15559ff9933f411b9a06f39a4a5
Dasha <konopelko@step.rivne>
2024-06-05 17:08:28 +0300

    intro

---
+++ 01_intro.py
@@ -0,0 +1,14 @@
+print("\t\t \\\\Hello\b \n\"Wo\'rld\"!!!\a",end=" ")
+print("HTML","CSS","JS","C++","C#",sep=" __ ")
+
+#print("\t \t lorem".upper())
+# print("\t \t Lorem".lower())
+# print("\t \t lorem ipsum".capitalize())
+# print("\t \t lorem ipsum".capitalize())
+# print("\t \t lorem ipsum".capitalize())
+print("\t \t \033[32m\033[41m\033[3m lorem  ipsum".capitalize())
+print("\033[0m",end="")
+print("\t \t lorem ipsum".capitalize())
+print("\t \t lorem ipsum".capitalize())
+
+# print("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare ut ex vel fermentum. Donec gravida nisi pharetra, facilisis massa sit amet, placerat sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam sit amet libero sed ipsum ullamcorper pellentesque. Nunc convallis diam at leo efficitur iaculis. Cras consequat lobortis leo quis mollis. Vivamus venenatis id erat vel congue. Quisque dignissim vel mi in ultricies. Aliquam scelerisque fringilla risus eget vehicula. Morbi sagittis blandit erat, sed convallis mi. Maecenas libero dui, pretium quis finibus in, finibus sit amet eros.")
